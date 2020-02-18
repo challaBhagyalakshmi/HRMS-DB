26b8268b0029b8ea9f2b08e20c948edc211accff
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-02-18 16:03:28 +0530

    Add index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const { resolve } = require('path');
+const processJson = require('./process/process.js');
+
+require('dotenv').config({
+  path: resolve(__dirname, './config/dev.env')
+});
+
+console.log(process.env.DB_USERNAME);
+
+const convert_to_json = require('./xlsx/parse_xlsx');
+
+const json = convert_to_json(
+  resolve(__dirname, './input/queries.xlsx'),
+  'All Operations'
+);
+
+processJson(json);
